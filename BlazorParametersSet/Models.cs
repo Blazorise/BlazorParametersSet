c13b96684795b8ee07dfc057fb7ce03d7740c098
Mladen Macanovic <mladen.macanovic@blazorise.com>
2025-11-26 09:05:41 +0100

    Add project files.

---
+++ BlazorParametersSet/Models.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorParametersSet
+{
+    public class ItemClass
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public override string ToString() => $"Class(Id={Id}, Name={Name})";
+    }
+
+    public record ItemRecord( int Id, string Name );
+}
